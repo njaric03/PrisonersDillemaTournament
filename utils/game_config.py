6351f65aeafcfd55cb3a045c07552f6b58545afb
njari <n.jaric03@gmail.com>
2025-01-09 18:40:37 +0100

    Refactor game simulation to remove hardcoded game round limits and update scoring system

--- utils/game_config.py
+++ utils/game_config.py
@@ -1,18 +1,18 @@
 class GameConfig:
     # Points awarded when both players cooperate
-    MUTUAL_COOPERATION_POINTS = 3
+    MUTUAL_COOPERATION_POINTS = 4
     
     # Points awarded to defector when other player cooperates
-    BETRAYAL_POINTS = 5
+    BETRAYAL_POINTS = 6
     
     # Points awarded to player who got betrayed
     BETRAYED_POINTS = 0
     
     # Points awarded when both players defect
-    MUTUAL_DEFECTION_POINTS = 1
+    MUTUAL_DEFECTION_POINTS = 2
     
     # Default number of rounds per match
-    NUMBER_OF_ROUNDS = 100
+    NUMBER_OF_ROUNDS = 200
 
     # Whether to add noise to number of rounds
     ADD_NOISE = False
